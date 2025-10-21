5f24bcc3897269e1f935dfef00742be69b8e6e65
José Geraldo da Costa <jgt.josegeraldo@gmail.com>
2025-10-21 06:52:56 -0300

    Alteração site

--- src/components/Header/mocks/index.ts
+++ src/components/Header/mocks/index.ts
@@ -9,25 +9,25 @@ export const sectionsLP: SecttionType[] = [
     name_section: 'O Evento',
     id: '1',
   },
-  {
+  /*{
     name_section: 'Inspire',
     id: '2',
-  },
+  },*/
   {
     name_section: 'Inscrições',
     id: '3',
   },
-  {
+  /*{
     name_section: 'Programação',
     id: '4',
-  },
+  },*/
   {
     name_section: 'Local',
     id: '5',
   },
-  {
+  /*{
     name_section: 'Parceiros',
     id: '6',
-  },
+  },*/
 ]
 
